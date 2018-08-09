4141daf27b251f03c6fc45523712cefcec160c9c
Proxy900 <32821943+Proxy900@users.noreply.github.com>
2018-08-08 17:10:29 -0700

    table sys, color sys
    More dynamic colors for each range of values in magnitude,
    Added a table to organize things better than just shoving everything into a div.

--- page.js
+++ page.js
@@ -1,16 +1,73 @@
 $(document).ready(function() {
   var json = $.getJSON('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson', function(data) {
     // console.log(data)
-    var quakes = {};
-    for (var i = 0; i < data.features.length; i++) {
-      quakes[i] = {};
-      quakes[i].place = data.features[i].properties.place;
-      quakes[i].mag = data.features[i].properties.mag;
-      quakes[i].details = "https://earthquake.usgs.gov/earthquakes/eventpage/" + data.features[i].properties.detail.slice(57, 67);
-      quakes[i].time = data.features[i].properties.time;
-      $("#quakes").append('<p><a href='+quakes[i].details+">"+quakes[i].place+": "+quakes[i].mag+"</a></p>");
-      $("p").attr('class', 'quake');
+    var quakes = [];
+
+    function Quake(plc, mg, dtl, tm) {
+      this.place = plc;
+      this.mag = mg;
+      this.details = dtl;
+      this.time = tm;
+      this.date = new Date(0);
+      this.date.setUTCSeconds(this.time);
+      this.desc = "";
     }
-    console.log(quakes);
+
+    for (var i = 0; i < data.features.length; i++) {
+      quakes.push(new Quake(data.features[i].properties.place, data.features[i].properties.mag,
+        "https://earthquake.usgs.gov/earthquakes/eventpage/" + data.features[i].properties.detail.slice(57, 67),
+        data.features[i].properties.time));
+
+      if (quakes[i].mag < 1) {
+        quakes[i].desc = "Less than Micro";
+      }
+
+      if (quakes[i].mag >= 1 && quakes[i].mag < 2) {
+        quakes[i].desc = "Micro";
+      } else if (quakes[i].mag >= 2 && quakes[i].mag < 4) {
+        quakes[i].desc = "Minor";
+      } else if (quakes[i].mag >= 4 && quakes[i].mag < 5) {
+        quakes[i].desc = "Light";
+      } else if (quakes[i].mag >= 5 && quakes[i].mag < 6) {
+        quakes[i].desc = "Moderate";
+      } else if (quakes[i].mag >= 6 && quakes[i].mag < 7) {
+        quakes[i].desc = "Strong";
+      } else if (quakes[i].mag >= 7 && quakes[i].mag < 8) {
+        quakes[i].desc = "Major";
+      } else if (quakes[i].mag > 8) {
+        quakes[i].desc = "Great";
+      }
+
+      $("#quakes").append('<tr class="quake"><td><a href='+quakes[i].details+'>'+quakes[i].place+'</a></td>'+'<td>'+quakes[i].mag+'</td>'+'<td>'+quakes[i].date+'</td>'+'<td>'+quakes[i].desc+'</td>'+'</tr>');
+      $("tr:last").attr('id', i);
+
+      if (quakes[i].mag > 1) {
+        $('#'+i).css('font-weight', '800');
+      }
+      
+      if (quakes[i].mag < 1) {
+        $('#'+i).css('background-color', '#c1d9ff');
+      }
+
+      if (quakes[i].mag >= 1 && quakes[i].mag < 2) {
+        $('#'+i).css('background-color', '#b8ffb2');
+      } else if (quakes[i].mag >= 2 && quakes[i].mag < 3) {
+        $('#'+i).css('background-color', '#d2ffb2');
+      } else if (quakes[i].mag >= 3 && quakes[i].mag < 4) {
+        $('#'+i).css('background-color', '#e6ffb2');
+      } else if (quakes[i].mag >= 4 && quakes[i].mag < 5) {
+        $('#'+i).css('background-color', '#f8ffb2');
+      } else if (quakes[i].mag >= 5 && quakes[i].mag < 6) {
+        $('#'+i).css('background-color', '#fff3b2');
+      } else if (quakes[i].mag >= 6 && quakes[i].mag < 7) {
+        $('#'+i).css('background-color', '#ffe2b2');
+      } else if (quakes[i].mag >= 7 && quakes[i].mag < 8) {
+        $('#'+i).css('background-color', '#ffc3b2');
+      } else if (quakes[i].mag >= 8 && quakes[i].mag < 9) {
+        $('#'+i).css('background-color', '#ffbdb2');
+      } else if (quakes[i].mag > 9) {
+        $('#'+i).css('background-color', '#ff6b6b');
+      }
+    }
   });
 });
